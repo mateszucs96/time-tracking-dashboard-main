a01d704be32c8ac94c1d368abaf27659dd98002c
mateszucs96 <szucsmate096@gmail.com>
2022-02-09 16:55:40 +0100

    final project

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const buttons = document.querySelectorAll('.btn');
+const secondaryTime = document.querySelectorAll('.secondary-time');
+const previous = document.querySelectorAll('.previous');
+const sectionContent = document.querySelectorAll('.section-content');
+const currentTime = document.querySelectorAll('.main-time');
+
+const buttonFunc = async function () {
+
+    const json = await fetch('data.json')
+    const data = await json.json()
+    console.log(data.length)
+
+    buttons.forEach(el => el.addEventListener('click', function () {
+        buttons.forEach(element => {
+            element.classList.remove('active')
+        });
+
+        el.classList.add('active');
+
+        if (el === buttons[0]) {
+            for (let i = 0; i < previous.length; i++) {
+                console.log(data[i].timeframes.daily)
+                secondaryTime[i].textContent = `Yesterday - ${data[i].timeframes.daily.previous}hrs`
+                currentTime[i].textContent = `${data[i].timeframes.daily.current}hrs`
+            }
+
+        }
+
+        if (el === buttons[1]) {
+            for (let i = 0; i < previous.length; i++) {
+                console.log(data[i].timeframes.weekly)
+                secondaryTime[i].textContent = `Last week - ${data[i].timeframes.weekly.previous}hrs`
+                currentTime[i].textContent = `${data[i].timeframes.weekly.current}hrs`
+            }
+
+        }
+
+        if (el === buttons[2]) {
+            for (let i = 0; i < previous.length; i++) {
+                console.log(data[i].timeframes.monthly)
+                secondaryTime[i].textContent = `Last month - ${data[i].timeframes.monthly.previous}hrs`
+                currentTime[i].textContent = `${data[i].timeframes.monthly.current}hrs`
+            }
+
+        }
+
+    }))
+
+
+}
+buttonFunc()
+
+
+
+
+
